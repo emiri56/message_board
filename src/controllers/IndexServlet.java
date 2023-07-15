6a9cfcbd6963e9a2c55d4a7b9298bb7e70aadba6
owner <owner@192.168.1.18>
2023-07-15 13:57:08 +0900

    フラッシュメッセージを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,4 +1,5 @@
 package controllers;
+
 import java.io.IOException;
 import java.util.List;
 
@@ -21,13 +22,21 @@ public class IndexServlet extends HttpServlet {
         super();
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
         List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
         em.close();
 
         request.setAttribute("messages", messages);
+
+        // フラッシュメッセージがセッションスコープにセットされていたら
+        // リクエストスコープに保存する（セッションスコープからは削除）
+        if (request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
         rd.forward(request, response);
