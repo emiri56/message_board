21a95310c9641e71876f13ccd9ef790221769646
owner <owner@192.168.1.18>
2023-07-15 13:26:21 +0900

    存在しないidに対する処理を追加

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -21,7 +21,8 @@ public class EditServlet extends HttpServlet {
         super();
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
         //該当のIDのメッセージ1件のみをデータベースから取得
@@ -29,11 +30,13 @@ public class EditServlet extends HttpServlet {
 
         em.close();
 
-     // メッセージ情報とセッションIDをリクエストスコープに登録
+        // メッセージ情報とセッションIDをリクエストスコープに登録
         request.setAttribute("message", m);
         request.setAttribute("_token", request.getSession().getId());
-     // メッセージIDをセッションスコープに登録
-        request.getSession().setAttribute("message_id", m.getId());
+        // メッセージIDをセッションスコープに登録
+        if (m != null) {
+            request.getSession().setAttribute("message_id", m.getId());
+        }
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
         rd.forward(request, response);
